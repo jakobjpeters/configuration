47470f9f44504169f1527e1d71e8b1ca42cd93ee
Jakob Peters <jakobj.peters@gmail.com>
2025-05-15 02:55:49 -0700

    Update `Startup.jl`

--- programs/julia/Startup.jl
+++ programs/julia/Startup.jl
@@ -2,110 +2,108 @@
 """
     Startup
 
-A module loaded in startup.jl.
-
-- Print initialization message
-- Set the `"EDITOR"` and `"SHELL"` `ENV`ironment variables.
-- Define and run `Startup.bar_cursor`
-- Define `Startup.toggle_precompile_workload`
-- If current path has a Project.toml file, load Revise.jl and the project into the `Main` module
-    - If the project is PAndQ.jl, run `install_atomize_mode`
-- Load OhMyREPL.jl and TerminalPager.jl into the `Startup` module
+A module loaded in `startup.jl`.
 """
 module Startup
-    import OhMyREPL, TerminalPager
-    using Base: Threads.@spawn, find_package
-    using Pkg: activate, develop, project
-    using Preferences: has_preference, load_preference, set_preferences!
-    using REPL: REPLCompletions
 
-    """
-        bar_cursor()
+import OhMyREPL, TerminalPager
+using Base: Threads.@spawn, find_package
+using Pkg: activate, develop, project
+using Preferences: has_preference, load_preference, set_preferences!
+using REPL: REPLCompletions
 
-    Set the terminal cursor to a steady bar `|`.
-    """
-    bar_cursor() = print("\e[6 q")
+"""
+    bar_cursor()
 
-    """
-        is_repl_ready()
-    """
-    is_repl_ready() = isdefined(Base, :active_repl_backend) && !isnothing(Base.active_repl_backend)
+Set the terminal cursor to a steady bar `|`.
+"""
+bar_cursor() = print("\e[6 q")
 
-    """
-        toggle_precompile_workload(package)
+"""
+    is_repl_ready()
+"""
+is_repl_ready() = isdefined(Base, :active_repl_backend) && !isnothing(Base.active_repl_backend)
 
-    Toggle whether the `package` runs its PrecompileTools.jl workload during precompilation.
-    """
-    function toggle_precompile_workload(package)
-        state = (
-            has_preference(package, "precompile_workload") &&
-            !load_preference(package, "precompile_workload")
-        )
-        set_preferences!(package, "precompile_workload" => state; force = true)
-        @info "Precompile workload for `$package` is $(state ? "en" : "dis")abled"
+"""
+    toggle_precompile_workload(package)
+
+Toggle whether the `package` runs its PrecompileTools.jl workload during precompilation.
+"""
+function toggle_precompile_workload(package)
+    state = (
+        has_preference(package, "precompile_workload") &&
+        !load_preference(package, "precompile_workload")
+    )
+    set_preferences!(package, "precompile_workload" => state; force = true)
+    @info "Precompile workload for `$package` is $(state ? "en" : "dis")abled"
+end
+
+"""
+    BarCursor
+"""
+struct BarCursor end
+
+(::BarCursor)(x) = :($bar_cursor(); $x)
+
+function __init__()
+    @info "`startup.jl` is running - see also `@doc Startup`"
+
+    bar_cursor()
+
+    for (key, value) in [
+        "JULIA_EDITOR" => "hx",
+        "JULIA_PKG_SERVER_REGISTRY_PREFERENCE" => "eager",
+        "JULIA_SHELL" => "bash"
+    ]
+        ENV[key] = value
     end
 
-    function __init__()
-        @info "startup.jl is running - see also `@doc Startup`"
+    # https://github.com/KristofferC/OhMyREPL.jl/issues/334#issuecomment-2485225896
+    @eval REPLCompletions close_path_completion(_, _, _, _) = false
 
-        bar_cursor()
+    if isfile("Project.toml")
 
-        for (key, value) in [
-            "JULIA_EDITOR" => "hx",
-            "JULIA_PKG_SERVER_REGISTRY_PREFERENCE" => "eager",
-            "JULIA_SHELL" => "bash"
-        ]
-            ENV[key] = value
-        end
+        activate(""; io = devnull)
+        name = project().name
 
-        # https://github.com/KristofferC/OhMyREPL.jl/issues/334#issuecomment-2485225896
-        @eval REPLCompletions close_path_completion(_, _, _, _) = false
+        if !isnothing(name)
+            isnothing(find_package(name)) && develop(; path = ".")
 
-        if isfile("Project.toml")
-            path = project().path
+            @eval begin
+                import Revise
 
-            activate(""; io = devnull)
-            name = project().name
-            activate(path; io = devnull)
+                """
+                    toggle_precompile_workload()
 
-            if !isnothing(name)
-                isnothing(find_package(name)) && develop(; path = ".")
+                Equivalent to `toggle_precompile_workload($($name))`.
+                """
+                toggle_precompile_workload() = toggle_precompile_workload($name)
+            end
 
-                @eval begin
-                    import Revise
+            @eval Main begin
+                using $(Symbol(name))
 
-                    """
-                        toggle_precompile_workload()
-
-                    Equivalent to `toggle_precompile_workload($($name))`.
-                    """
-                    toggle_precompile_workload() = toggle_precompile_workload($name)
-                end
-
-                @eval Main begin
-                    using $(Symbol(name))
-
-                    if $name == "PAndQ"
-                        install_atomize_mode()
-                        @eval @variables p q
-                    elseif $name == "Speculator"
-                        install_speculator(; limit = 4, verbosity = debug)
-                    end
+                if $name == "PAndQ"
+                    install_atomize_mode()
+                    @eval @variables p q
+                elseif $name == "Speculator"
+                    install_speculator(; limit = 4, verbosity = debug)
                 end
             end
         end
+    end
 
-        @spawn begin
-            _time = time()
+    @spawn begin
+        _time = time()
 
-            while !(repl_ready = is_repl_ready()) && time() - _time < 10
-                sleep(0.1)
-            end
+        while !(repl_ready = is_repl_ready()) && time() - _time < 10
+            sleep(0.1)
+        end
 
-            if repl_ready
-                push!(Base.active_repl_backend.ast_transforms, x -> :($bar_cursor(); $x))
-            else error("Timed out waiting for REPL to load")
-            end
+        if repl_ready push!(Base.active_repl_backend.ast_transforms, BarCursor())
+        else error("Timed out waiting for REPL to load")
         end
     end
 end
+
+end # module
